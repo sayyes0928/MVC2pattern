5a58433ac30b5b88216903a944545888b83455e9
Administrator <Administrator@321-9>
2020-08-07 18:23:53 +0900

    aaaaa

--- src/com/jachi/Action/TiplistAction.java
+++ src/com/jachi/Action/TiplistAction.java
@@ -9,7 +9,7 @@ public class TiplistAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+		// TODO Auto-generated method stub
 		return null;
 	}
 
