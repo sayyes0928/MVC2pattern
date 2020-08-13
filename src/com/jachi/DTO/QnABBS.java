894c6404f2de34e2317f8ac27c77e6fca41c4973
Administrator <Administrator@321-10>
2020-08-13 18:26:04 +0900

    push push baby

--- src/com/jachi/DTO/QnABBS.java
+++ src/com/jachi/DTO/QnABBS.java
@@ -1,42 +1,57 @@
 package com.jachi.DTO;
 
 public class QnABBS {
-int qna_num;
-public int getQna_num() {
-	return qna_num;
-}
-public void setQna_num(int qna_num) {
-	this.qna_num = qna_num;
-}
-public String getQna_title() {
-	return qna_title;
-}
-public void setQna_title(String qna_title) {
-	this.qna_title = qna_title;
-}
-public String getQna_nickname() {
-	return qna_nickname;
-}
-public void setQna_nickname(String qna_nickname) {
-	this.qna_nickname = qna_nickname;
-}
-public String getQna_time() {
-	return qna_time;
-}
-public void setQna_time(String qna_time) {
-	this.qna_time = qna_time;
-}
 
-public String getQna_post() {
-	return qna_post;
-}
-public void setQna_post(String qna_post) {
-	this.qna_post = qna_post;
-}
-
-
-String qna_post;
-String qna_title;
-String qna_nickname;
-String qna_time;
+	String qna_nickname;
+	String qna_post;
+	String qna_pw;
+	int qna_num;
+	String qna_img;
+	String qna_time;
+	String qna_title;
+	public String getQna_nickname() {
+		return qna_nickname;
+	}
+	public void setQna_nickname(String qna_nickname) {
+		this.qna_nickname = qna_nickname;
+	}
+	public String getQna_post() {
+		return qna_post;
+	}
+	public void setQna_post(String qna_post) {
+		this.qna_post = qna_post;
+	}
+	public String getQna_pw() {
+		return qna_pw;
+	}
+	public void setQna_pw(String qna_pw) {
+		this.qna_pw = qna_pw;
+	}
+	public int getQna_num() {
+		return qna_num;
+	}
+	public void setQna_num(int qna_num) {
+		this.qna_num = qna_num;
+	}
+	public String getQna_img() {
+		return qna_img;
+	}
+	public void setQna_img(String qna_img) {
+		this.qna_img = qna_img;
+	}
+	public String getQna_time() {
+		return qna_time;
+	}
+	public void setQna_time(String qna_time) {
+		this.qna_time = qna_time;
+	}
+	public String getQna_title() {
+		return qna_title;
+	}
+	public void setQna_title(String qna_title) {
+		this.qna_title = qna_title;
+	}
+	
+	
+	
 }
